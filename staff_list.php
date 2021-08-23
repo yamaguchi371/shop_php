3c04f5a27f2e046b05f776e2d3577c798a2e4e2a
yamaguchi371 <telework-takefu6@outlook.jp>
2021-08-23 14:08:34 +0900

    staff_list,staff_edit追加

--- staff_list.php
+++ staff_list.php
@@ -9,34 +9,40 @@
 <body>
     <?php
         try {
-            $staff_name = $_POST['name'];
-            $staff_pass = $_POST['pass'];
-
-            $staff_name = htmlspecialchars($staff_name, ENT_QUOTES, 'UTF-8');
-            $staff_pass = htmlspecialchars($staff_pass, ENT_QUOTES, 'UTF-8');
-
+            
             $dsn = 'mysql:dbname=shop;host=localhost;charset=utf8';
             $user = 'root';
             $password = 'tooled371';
             $dbh = new PDO($dsn, $user, $password);
             $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-            $sql = 'INSERT INTO mst_staff(name, password) VALUES (?,?)';
+            $sql = 'SELECT code, name FROM mst_staff WHERE 1';
             $stmt = $dbh->prepare($sql);
-            $data[] = $staff_name;
-            $data[] = $staff_pass;
-            $stmt->execute($data);
+            $stmt->execute();
 
             $dbh = null;
 
-            print $staff_name;
-            print 'さんを追加しました。<br>';
+            print 'スタッフ一覧<br><br>';
+
+            print '<form method="post" action="staff_edit.php">';
+
+            while(true) {
+                $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+                if($rec==false) {
+                    break;
+                }
+                print '<input type="radio" name="staffcode" value="'.$rec['code'].'">';
+                print $rec['name'];
+                print '<br>';
+            }
+
+            print '<input type="submit" value="修正">';
+            print '</form>';
+
         } catch (Exception $e) {
             print 'ただいま障害により大変ご迷惑をお掛けしております。';
             exit();
         }
     ?>
-
-    <a href="staff_list.php">戻る</a>
 </body>
 </html>
